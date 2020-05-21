777843607c395646538959a3ddeb4f8a31d39ae2
Kamil Rominski <s20599@pjwstk.edu.pl>
2020-05-22 01:07:26 +0200

    Added class ScoreBoard with placeholder for score Added lombok to project

--- src/com/program/MainWindow.java
+++ src/com/program/MainWindow.java
@@ -1,21 +1,28 @@
 package com.program;
 
-import com.sun.tools.javac.Main;
-
+import lombok.Getter;
 import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow {
+    @Getter
+    private Dimension screenSize;
+
+    public void setScreenSize() {
+        this.screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    }
+
     public MainWindow(){
         //Declaration of variables and new objects
         JFrame mainWindow = new JFrame();
         JPanel panel = new JPanel();
         Grid grid = new Grid();
         ScoreBoard scoreBoard = new ScoreBoard();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenWidth = (int)screenSize.getWidth();
-        int screenHeight = (int)screenSize.getHeight();
+        setScreenSize();
+        int screenWidth = (int)getScreenSize().getWidth();
+        int screenHeight = (int)getScreenSize().getHeight();
         //Creating window and setting it's parameters
+        mainWindow.setTitle("2048 by Kamil Rominski");
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainWindow.setLocation(screenWidth/3,screenHeight/10);
         mainWindow.setSize(new Dimension(600,800));
@@ -23,13 +30,14 @@ public class MainWindow {
         mainWindow.setVisible(true);
         //Adding content to the frame and panel
         grid.setSize(new Dimension(600,800));
-        //panel.add(scoreBoard);
+        scoreBoard.setSize(new Dimension(600,800));
+        panel.add(scoreBoard);
         panel.setLayout(null);
         panel.add(grid);
         mainWindow.setContentPane(panel);
     }
+
     public static void main(String[] args){
         new MainWindow();
-
     }
 }
--- src/com/program/ScoreBoard.java
+++ src/com/program/ScoreBoard.java
@@ -4,13 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ScoreBoard extends JPanel {
-    public void paintComponent(Graphics g){
-        g.setColor(Color.BLUE);
-        g.fillRoundRect(5,10,15,25,35,45);
-        g.setColor(Color.BLACK);
-        g.drawString("2",5,5);
-    }
+
     public void Update(){
         repaint();
+    }
+    public void paintComponent(Graphics g){
+        g.setColor(Color.BLUE);
+        g.fillRoundRect(340,30,200,50,10,10);
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN,35));
+        g.drawString("Score: 0",350,65);
     }
 }
